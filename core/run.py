5e5bae35a39f99e2271ccfd847165e1b2ca23446
dimmg <dumitru.gira@gmail.com>
2016-11-26 03:11:04 +0200

    feat(core): add gunicorn

--- core/run.py
+++ core/run.py
@@ -1,5 +1,22 @@
+import os
+
+from werkzeug.contrib.fixers import ProxyFix
+
 from api import create_app
 
 
+app = create_app()
+
+def run():
+    debug = os.environ.get('APP_DEBUG', True)
+    host = os.environ.get('APP_HOST', '0.0.0.0')
+    port = int(os.environ.get('APP_PORT', 5000))
+
+    app.run(debug=debug, host=host, port=port)
+
+
+wsgi = ProxyFix(app.wsgi_app)
+
+
 if __name__ == '__main__':
-    create_app().run(debug=True, host='0.0.0.0')
+    run()
